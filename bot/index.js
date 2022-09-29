1b96efae8ac90bc3c857d4cbcddf44758a3fd1aa
Lorenzobattistela <lorenzowottrich@gmail.com>
2022-09-29 18:14:08 -0300

    getting all channel messages

--- bot/index.js
+++ bot/index.js
@@ -3,7 +3,8 @@ const { Client, GatewayIntentBits } = require('discord.js');
 const { bot_token } = require('../config/auth.json');
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const messages = [];
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
@@ -16,7 +17,10 @@ client.on('interactionCreate', async interaction => {
 	const { commandName } = interaction;
     channelId = interaction.options.getString('input');
 	if (commandName == 'get') {
-        console.log(client.channels.cache.find(channel=> channel.id == channelId))
+        channel = client.channels.cache.find(channel=> channel.id == channelId);
+        channel.messages.fetch().then(messages => {
+            messages.forEach(message => messages.append(message.content))
+        })
 		await interaction.reply('Pong!');
 	} else {
         return;
