3619bb0d5cd739cee6aab0663e9181443b806548
Lorenzobattistela <lorenzowottrich@gmail.com>
2022-10-03 19:43:51 -0300

    retrieving messages from db from model

--- model/helpers.py
+++ model/helpers.py
@@ -1,4 +1,6 @@
+from distutils.log import error
 from typing import List, Tuple
+import sqlite3
 
 def generateTrainingData() -> List[Tuple]:
   training_data = []
@@ -6,7 +8,6 @@ def generateTrainingData() -> List[Tuple]:
   # len tech = 12
   phrases = ["Há anos que trabalho com ", "Venho estudando e aplicando no trabalho o ", "Meu emprego envolve tecnologias como ", "Nos últimos anos, trabalhei em projetos com ", "Estou aprendendo a usar ", "Meus estudos envolvem ", "Mexo com ", "Estou estagiando e uso ", "No meu estágio, aprendi "]
   # phrases = 9
-
 
   for phrase in phrases:
     for tech in technologies:
@@ -16,3 +17,13 @@ def generateTrainingData() -> List[Tuple]:
       training_data.append((train_phr, [(idxStart, idxEnd, "TECH")]))
 
   return training_data
+
+def getMessages(limit: int=100) -> List[str]:
+    con = sqlite3.connect("db/messages.db")
+    cur = con.cursor()
+    query = f"SELECT text FROM messages LIMIT {limit};"
+    res = cur.execute(query)
+    messages = res.fetchall()
+    return [msg[0] for msg in messages]
+
+getMessages()
