e26706fc4cc9b173076b813988f5263e40f2a2c9
Lorenzobattistela <lorenzowottrich@gmail.com>
2022-10-06 19:12:32 -0300

    adding labeled data and cleaning it

--- model/helpers.py
+++ model/helpers.py
@@ -3,21 +3,44 @@ from typing import List, Tuple
 import sqlite3
 import json
 
-def generateTrainingData() -> List[Tuple]:
-  training_data = []
-  technologies = ["react", "javascript", "python", "c", "c#", "elixir", "desenvolvimento web", "html", "css", "angular", "vue", "haskell", "solidity", "blockchain"]
-  # len tech = 12
-  phrases = ["Há anos que trabalho com ", "Venho estudando e aplicando no trabalho o ", "Meu emprego envolve tecnologias como ", "Nos últimos anos, trabalhei em projetos com ", "Estou aprendendo a usar ", "Meus estudos envolvem ", "Mexo com ", "Estou estagiando e uso ", "No meu estágio, aprendi "]
-  # phrases = 9
 
-  for phrase in phrases:
-    for tech in technologies:
-      train_phr = f"{phrase}{tech}"
-      idxStart = train_phr.find(tech)
-      idxEnd = idxStart + len(tech)
-      training_data.append((train_phr, [(idxStart, idxEnd, "TECH")]))
+# def generateTrainingData(all_data: List[object] = []) -> List[Tuple]:
+#   training_data = []
+#   for data in all_data:
+#     training_data.append((data[""]))
+  
+  
+  
+  
 
-  return training_data
+#   for phrase in phrases:
+#     for tech in technologies:
+#       train_phr = f"{phrase}{tech}"
+#       idxStart = train_phr.find(tech)
+#       idxEnd = idxStart + len(tech)
+#       training_data.append((train_phr, [(idxStart, idxEnd, "TECH")]))
+#   return training_data
+
+def cleanData(all_data: List=[]) -> List[str]:
+  cleaned = []
+  print(all_data[1])
+  print(all_data[1])
+  for data in all_data:
+    text = data["data"]["text"]
+    data_id = data["id"]
+    annotations_dirty = data["annotations"][0]["result"]
+    annotations = [an["value"] for an in annotations_dirty]
+   
+    cleaned_obj = {
+      "data_id": data_id,
+      "original_text": text,
+      "annotations": annotations
+    }
+    cleaned.append(cleaned_obj)
+
+  print(cleaned)
+  return cleaned
+
 
 def getMessages(limit: int=100) -> List[str]:
     con = sqlite3.connect("db/messages.db")
@@ -27,9 +50,11 @@ def getMessages(limit: int=100) -> List[str]:
     messages = res.fetchall()
     return [msg[0] for msg in messages]
   
-def getJson() -> List[Tuple]:
-  with open("./data/labeled.json") as f:
+def getJson(filename) -> List[Tuple]:
+  with open(f"./data/{filename}.json") as f:
     file_contents = f.read()
 
   parsed_json = json.loads(file_contents)
   return parsed_json
+
+cleanData(getJson("labeled"))
