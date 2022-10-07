a121c451e211abefdabbb83c0fb3531db444e746
Lorenzobattistela <lorenzowottrich@gmail.com>
2022-10-06 21:04:19 -0300

    remaking model

--- model/model.py
+++ model/model.py
@@ -1,30 +1,47 @@
-from helpers import generateTrainingData
+from helpers import generateTrainingData, getJson, cleanData
 import random
 import spacy 
 from spacy.training import Example
+from pathlib import Path
+from tqdm import tqdm
 
-nlp = spacy.blank('pt')  # new, empty model. Let’s say it’s for the English language
-nlp.vocab.vectors.name = 'example_model_training'   # give a name to our list of vectors
-# add NER pipeline
-# our pipeline would just do NER
-nlp.add_pipe('ner', last=True)  # we add the pipeline to the model
+model = None
+n_iter=100
+output_dir = Path("/saved_model")
 
-ner = nlp.get_pipe("ner")
+# Checks to see if there is a current model or no model. In this case I will be starting with a blank model 
+if model is not None:
+    ner_model = spacy.load(model)  # load existing spaCy model
+    print("Loaded model '%s'" % model)
+else:
+# this will create a blank english model
+  ner_model = spacy.blank('pt')  # create blank Language class
+  print("Created blank 'pt' model")
 
-DATA = generateTrainingData()
+TRAIN_DATA = generateTrainingData(cleanData(getJson('labeled')))
 
-ner.add_label("TECH")
-
-optimizer = nlp.begin_training()
-
-for i in range(20):
-    random.shuffle(DATA)
-    for text, annotations in DATA:
-        doc = nlp.make_doc(text)
-        example = Example.from_dict(doc, {"entities": annotations})
-        nlp.update([example], sgd=optimizer)
-
-doc = nlp("Olá pessoal! Meu nome é Lucas e sou um desenvolvedor web buscando aprender sobre web3, solidity e blockchain.")
-
-for entity in doc.ents:
-  print(entity.label_, ' | ', entity.text)
+ner_model.add_pipe('ner')
+ner = ner_model.get_pipe('ner')
+for _, annotations in TRAIN_DATA:
+    for ent in annotations:
+      ner.add_label(ent[2])
+      
+other_pipes = [pipe for pipe in ner_model.pipe_names if pipe != 'ner']
+with ner_model.disable_pipes(*other_pipes):  # only train NER
+    optimizer = ner_model.begin_training()
+    for itn in range(n_iter):
+        random.shuffle(TRAIN_DATA)
+        losses = {}
+        for text, annotations in tqdm(TRAIN_DATA):
+            doc = ner_model.make_doc(text)
+            example = Example.from_dict(doc, {"entities": annotations})
+            ner_model.update(
+                [example],
+                drop=0.5,  # dropout 
+                sgd=optimizer,  # callable to update weights
+                losses=losses)
+        print(losses)
+      
+for text, _ in TRAIN_DATA:
+  doc = ner_model('Meu nome é Lorenzo, trabalho com Javascript no Brasil.')
+  print('Entities', [(ent.text, ent.label) for ent in doc.ents])
