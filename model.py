b0b3015138ebee60aecb5a6502dcc6cc5040e715
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-02-27 19:32:56 -0300

    [feat] model tuning

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 class Model:
   def __init__(self, prompts: List) -> None:
     self.prompts = prompts
-    self.base_prompt = """A mensagem a seguir é um texto de apresentação. Extraia desse texto os campos: emprego, nome e tecnologias. Retorne esses dados no formato JSON, com os rótulos: job, name, techs. Cada rótulo corresponde a uma lista de strings, onde irão os dados extraídos. Caso identifique que o usuário é um estudante também, adicione estudante ao campo job. Caso não encontre os campos especificados, deixe o atributo em branco. Retorne apenas o JSON, sem nenhum outro texto na resposta.\n"""
+    self.base_prompt = """A mensagem a seguir é um texto de apresentação. Extraia desse texto os campos: emprego, nome, cidade, estado e tecnologias. Retorne esses dados no formato JSON, com os rótulos: job, name, city, state, techs. Cada rótulo corresponde a uma lista de strings, onde irão os dados extraídos. Caso identifique que o usuário é um estudante também, adicione estudante ao campo job. Caso não encontre os campos especificados, deixe o atributo em branco. Retorne apenas o JSON, sem nenhum outro texto na resposta.\n"""
   
   def extract_from_all_prompts(self) -> List:
     responses = []
@@ -19,10 +19,10 @@ class Model:
       response = openai.Completion.create(
         model="text-davinci-003",
         prompt=final_prompt,
-        temperature=0.5,
-        max_tokens=150,
+        temperature=0.1,
+        max_tokens=250,
         top_p=1,
-        frequency_penalty=0.8,
+        frequency_penalty=0,
         presence_penalty=0
       )
       responses.append(response)
