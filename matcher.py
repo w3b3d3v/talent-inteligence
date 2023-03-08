7275c8bcaa3aa0298e9917a2ee7571a21d1e4ed2
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-03-08 09:15:57 -0300

    [feat] process prompts with matcher first and then the model to save resources

---
+++ matcher.py
@@ -0,0 +1,51 @@
+from typing import List, Tuple, Dict
+
+class Matcher:
+    def __init__(self, techs: List, jobs: List, names: List, uf: List) -> None:
+        self.techs = techs
+        self.jobs = jobs
+        self.names = names
+        self.uf = uf
+        self.ai_prompts = []
+        self.last_id_index = None
+    
+    def match_prompt(self, prompt: str) -> Tuple:
+        matched_techs = []
+        matched_jobs = []
+
+        for tech in self.techs:
+            if tech in prompt.lower():
+                matched_techs.append(tech)
+        # enqueue prompts that did not matched so that the AI can continue
+        for job in self.jobs:
+            if job in prompt.lower():
+                matched_jobs.append(job)
+        
+        for name in self.names:
+            if name in prompt.lower():
+                matched_name = name
+        
+        for uf in self.uf:
+            if uf in prompt.lower():
+                matched_uf = uf
+                
+        
+        if len(matched_techs) == 0 and len(matched_jobs) == 0:
+            self.ai_prompts.append(prompt)
+        return (matched_jobs, matched_techs, matched_name or None, matched_uf or None)
+    
+    def to_json(self, matches: Tuple) -> Dict:
+        try:
+            return {
+                "jobs": matches[0],
+                "techs": matches[1],
+                "name": matches[2],
+                "uf": matches[3]
+            } 
+        except:
+            return {}
+
+    def get_ai_prompts(self) -> List[str]:
+        return self.ai_prompts
+
+    
