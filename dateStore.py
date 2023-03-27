d70ae4947989ad84dd2bdde2f062688dfcc4ee91
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-03-27 12:14:48 -0300

    [refactor] receiving filename to help on testing

--- dateStore.py
+++ dateStore.py
@@ -7,16 +7,16 @@ class LastDate():
         self.last_date = last_date
 
 
-def save_last_date(obj: LastDate):
+def save_last_date(obj: LastDate, filename: str = "last_date.pkl"):
     try:
-        with open("last_date.pickle", "wb") as f:
+        with open(filename, "wb") as f:
             pickle.dump(obj=obj, file=f, protocol=pickle.HIGHEST_PROTOCOL)
     except Exception as e:
         print(e)
 
-def load_last_date():
+def load_last_date(filename: str = "last_date.pkl"):
     try:
-        with open("last_date.pickle", "rb") as f:
+        with open(filename, "rb") as f:
             return pickle.load(f)
     except Exception as e:
         print(e)
