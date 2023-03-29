37c0fc7d9be746a6d9882358acb8dc43f25176e7
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-03-29 12:01:43 -0300

    [chore] flake8 style fix

--- matcher.py
+++ matcher.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple, Dict
 
+
 class Matcher:
     def __init__(self, techs: List, jobs: List, names: List, uf: List) -> None:
         self.techs = techs
@@ -8,7 +9,7 @@ class Matcher:
         self.uf = uf
         self.ai_prompts = []
         self.last_id_index = None
-    
+
     def match_prompt(self, prompt: str) -> Tuple:
         matched_techs = []
         matched_jobs = []
@@ -22,21 +23,20 @@ class Matcher:
         for job in self.jobs:
             if job in prompt.lower():
                 matched_jobs.append(job)
-        
+
         for name in self.names:
             if name in prompt.lower():
                 matched_name = name
-        
+
         for uf in self.uf:
             if uf in prompt.lower():
                 matched_uf = uf
-                
-        
+
         if len(matched_techs) == 0 or len(matched_jobs) == 0:
             self.ai_prompts.append(prompt)
             return ()
         return (matched_jobs, matched_techs, matched_name or "", matched_uf or "")
-    
+
     def to_json(self, matches: Tuple) -> Dict:
         try:
             return {
@@ -44,11 +44,9 @@ class Matcher:
                 "techs": matches[1],
                 "name": matches[2],
                 "uf": matches[3]
-            } 
-        except:
+            }
+        except Exception:
             return {}
 
     def get_ai_prompts(self) -> List[str]:
         return self.ai_prompts
-
-    
