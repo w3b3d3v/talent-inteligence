c45a32163c1cafb94573327c031d4398deeb97d2
Lorenzobattistela <lorenzobattistela@gmail.com>
2023-08-04 16:08:21 -0300

    [fix] flake8 match

--- test_matcher.py
+++ test_matcher.py
@@ -8,9 +8,11 @@ matcher = Matcher(
     uf=["rio grande do sul", "são paulo"]
 )
 
+
 def test_ai_prompts_getter():
     ai_prompts = matcher.get_ai_prompts()
     assert isinstance(ai_prompts, list), "ai_prompts should be a list"
+
 
 def test_match_prompt():
     prompt = "Meu nome é rafael, sou desenvolvedor e utilizo javascript. Moro no rio grande do sul."
@@ -21,10 +23,12 @@ def test_match_prompt():
     assert isinstance(match[2], str), "matched name should be a string"
     assert isinstance(match[3], str), "matched uf should be a string"
 
+
 def test_match_prompt_no_matches():
     prompt = "I am an artist living in New York."
     match = matcher.match_prompt(prompt=prompt)
     assert len(match) == 0, "No matches should be found"
+
 
 def test_to_json():
     match = (["desenvolvedor"], ["javascript"], "rafael", "rio grande do sul")
@@ -35,10 +39,12 @@ def test_to_json():
     assert "state" in result
     assert "city" in result
 
+
 def test_to_json_empty_match():
     match = ()
     result = matcher.to_json(match)
     assert result == {}
+
 
 if __name__ == "__main__":
     pytest.main()
